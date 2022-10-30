8753096142219bf687c294038b1dc0470932da5e
robin marillia <robin4a4@gmail.com>
2022-10-30 21:52:30 +0100

    add spa

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,17 +1,24 @@
-import { error } from '@sveltejs/kit';
-import { api } from './api';
-import type { LayoutServerLoad, Actions } from './$types';
+import { error } from "@sveltejs/kit";
+import { api } from "./api";
+import type { LayoutServerLoad, Actions } from "./$types";
 import type { Link, Category } from "@prisma/client";
 
-export const load: LayoutServerLoad = async ({params}) => {
-	const response = await api('GET', {categoryName: params.categoryName});
-	if (response.status === 404) {
-		return {
-			links: [],
-			categories: [],
-            currentCategory: params.categoryName
-		};
-	}
-	if (response.status === 200) return {...response.body, currentCategory: params.categoryName} as {links: Link[], categories: Category[], currentCategory: string};
-	throw error(response.status);
+export const ssr = false;
+
+export const load: LayoutServerLoad = async ({ params }) => {
+  const response = await api("GET", { categoryName: params.categoryName });
+  if (response.status === 404) {
+    return {
+      links: [],
+      categories: [],
+      currentCategory: params.categoryName,
+    };
+  }
+  if (response.status === 200)
+    return { ...response.body, currentCategory: params.categoryName } as {
+      links: Link[];
+      categories: Category[];
+      currentCategory: string;
+    };
+  throw error(response.status);
 };
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,13 @@
-import { api } from './api';
-import type { Actions } from './$types';
+import { api } from "./api";
+import type { Actions } from "./$types";
 import type { Link } from "@prisma/client";
 
 export const actions: Actions = {
-	add: async ({ request }) => {
-		console.log(request.headers.get('user-agent'))
-		const form = await request.formData();
-		const formAsObj = Object.fromEntries(form as any); // doing that because dom.iterable does not work in tsconfig.
-		formAsObj.categoryId = parseInt(formAsObj.categoryId, 10);
-		const data = formAsObj as Link
-		await api('POST', {data});
-	},
+  add: async ({ request }) => {
+    const form = await request.formData();
+    const formAsObj = Object.fromEntries(form as any); // doing that because dom.iterable does not work in tsconfig.
+    formAsObj.categoryId = parseInt(formAsObj.categoryId, 10);
+    const data = formAsObj as Link;
+    await api("POST", { data });
+  },
 };
